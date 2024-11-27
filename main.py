cdba02b267934f153a9db09aa6ffb1c7aafb34c9
Nataniel Soares <contatonatanielsoares@gmail.com>
2024-11-27 09:39:45 -0300

    Removendo arquivos da pasta do controle de versão

--- main.py
+++
@@ -1,7 +0,0 @@
-from baixar_musicas import BaixarMusicas
-from playlist import Playlist
-
-playlist = Playlist('https://www.youtube.com/watch?v=9tM7xr7qeho&list=RDQMXwQAo6fqxwA&start_radio=1')
-list_of_urls = playlist.pegar_links_playlist()
-bm = BaixarMusicas(list_of_urls)
-bm.baixar_todos_videos()
