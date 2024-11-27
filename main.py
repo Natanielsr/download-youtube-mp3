cb10423dcb57d5db91e24b679980e22b487d6a29
Nataniel Soares <contatonatanielsoares@gmail.com>
2024-11-27 10:05:05 -0300

    url da playlist

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from baixar_musicas import BaixarMusicas
 from playlist import Playlist
 
-playlist = Playlist('https://www.youtube.com/watch?v=9tM7xr7qeho&list=RDQMXwQAo6fqxwA&start_radio=1')
+playlist = Playlist('[coloque_aqui_a_url_da_playlist_que_voce_deseja_baixar]')
 list_of_urls = playlist.pegar_links_playlist()
 bm = BaixarMusicas(list_of_urls)
 bm.baixar_todos_videos()
